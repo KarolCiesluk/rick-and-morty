66a97b94abc366aa9bd9b2cc3915d17e0ae491a4
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-01-19 12:16:41 +0100

    import new components to CharacterList

--- src/CharacterList/index.tsx
+++ src/CharacterList/index.tsx
@@ -1,27 +1,35 @@
+import Header from "./Header";
 import { Character } from "./interfaces";
+import Pagination from "./Pagination";
 import useCharactersData from "./useCharactersData";
+import { useQueryParameter } from "./useQueryParameter";
 
 const CharacterList = () => {
-  const [status, charactersData] = useCharactersData();
+  const currentPage = parseInt(useQueryParameter("page") || "1");
+  const [status, charactersData] = useCharactersData(currentPage);
 
   return (
     <>
-      <header>
-        <h1>Rick and Morty app</h1>
-      </header>
+      <Header>Rick and Morty app</Header>
       <main>
         <h2>Character List</h2>
         {!!charactersData &&
-          <ul>
-            {charactersData.results.map(({ id, image, name, status, species }: Character) => (
-              <li key={id}>
-                <img src={image} alt="" />
-                <h3>{name}</h3>
-                <p>Status: {status}</p>
-                <p>Species: {species}</p>
-              </li>
-            ))}
-          </ul>
+          <>
+            <ul>
+              {charactersData.results.map(({ id, image, name, status, species }: Character) => (
+                <li key={id}>
+                  <img src={image} alt="" />
+                  <h3>{name}</h3>
+                  <p>Status: {status}</p>
+                  <p>Species: {species}</p>
+                </li>
+              ))}
+            </ul>
+            <Pagination
+              lastPage={charactersData.info.pages}
+              currentPage={currentPage}
+            />
+          </>
         }
       </main>
     </>
