3df3da3b75bab648993e9ccd880c8d634e2650db
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-01-22 11:52:10 +0100

    move interfaces to API file

--- src/components/CharacterList/API.ts
+++ src/components/CharacterList/API.ts
@@ -12,3 +12,23 @@ export const getCharactersData = async (signal: AbortSignal, page: number) => {
 
   return await response.json();
 };
+
+export interface Info {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+}
+
+export interface Character {
+  id: number;
+  image: string;
+  name: string;
+  species: string;
+  status: "Dead" | "Alive" | "unknown";
+}
+
+export interface CharactersData {
+  info: Info;
+  results: Character[];
+}
